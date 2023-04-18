955fbc343b4079d6471ed476c31475f0848c8215
Moses Hunsu <hunsumoses7@gmail.com>
2023-04-18 11:52:36 +0100

    Shapes Exercise using OOP

--- src/abstraction_polymorphism_interface/shapeHierarchy/Shape.java
+++ src/abstraction_polymorphism_interface/shapeHierarchy/Shape.java
@@ -1,8 +1,5 @@
 package abstraction_polymorphism_interface.shapeHierarchy;
 
-import abstraction_polymorphism_interface.shapeHierarchy.interfaces.GetArea;
-import abstraction_polymorphism_interface.shapeHierarchy.interfaces.GetVolume;
-
 public abstract class Shape {
     public abstract double getArea();
 }
--- src/abstraction_polymorphism_interface/shapeHierarchy/interfaces/GetArea.java
+++
@@ -1,5 +0,0 @@
-package abstraction_polymorphism_interface.shapeHierarchy.interfaces;
-
-public interface GetArea {
-    double getArea();
-}
