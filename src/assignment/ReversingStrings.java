83c5c31e53e7ef1be5104873d77c1141b0e948e3
Moses Hunsu <hunsumoses7@gmail.com>
2023-08-14 23:22:47 +0100

    Recursion

---
+++ src/assignment/ReversingStrings.java
@@ -0,0 +1,10 @@
+package assignment;
+
+import java.util.Stack;
+
+public class ReversingStrings {
+
+    public static String reversingStrings(String str) {
+        return "";
+    }
+}
---
+++ src/recurssion/RecTest.java
@@ -0,0 +1,19 @@
+package recurssion;
+
+public class RecTest {
+    public static void main(String[] args) {
+        System.out.println(reverseNumberIteration(435));
+        System.out.println(reverseNumberIteration(1224));
+    }
+
+    public static int reverseNumberIteration(int n) {
+        int reversal = 0;
+        while (n > 0) {
+            int remainder = n % 10;
+            reversal = reversal * 10 + remainder;
+            n = n / 10;
+        }
+        return reversal;
+    }
+
+}
